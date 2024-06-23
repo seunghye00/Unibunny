e43bacecec2b3e28608309d346dab2e5c0ace6ab
seunghye00 <tmdgp1934@gmail.com>
2024-06-23 18:50:24 +0900

    ReplyDAO 파일 변경
    좋아요 수 반환 메서드 추가 및 sql 문법 오류 수정

--- src/main/java/dao/ReplyDAO.java
+++ src/main/java/dao/ReplyDAO.java
@@ -11,12 +11,14 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import com.google.gson.JsonElement;
+
 import dto.ReplyDTO;
 
 public class ReplyDAO {
-	
+
 	private static ReplyDAO instance;
-	
+
 	public static synchronized ReplyDAO getInstance() {
 		if (instance == null) {
 			instance = new ReplyDAO();
@@ -24,7 +26,8 @@ public class ReplyDAO {
 		return instance;
 	}
 
-	private ReplyDAO() {}
+	private ReplyDAO() {
+	}
 
 	private Connection getConnection() throws Exception {
 		Context ctx = new InitialContext();
@@ -32,32 +35,35 @@ public class ReplyDAO {
 		return ds.getConnection();
 	}
 
-	// target_seq가 board_seq인 댓글 목록 조회
-	public List<ReplyDTO> selectByBoardSeq(int target_seq) throws Exception {
+	// board_seq로 해당 게시물의 댓글 목록 조회 및 매개변수로 정렬 기준 설정
+	public List<ReplyDTO> selectByBoardSeq(int board_seq, String order_by) throws Exception {
 
-		String sql = "select * from reply where board_seq = ?";
-
-		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
-			pstat.setInt(1, target_seq);
-			List<ReplyDTO> list = new ArrayList<>();
-			try (ResultSet rs = pstat.executeQuery();) {
-				while (rs.next()) {
-					int seq = rs.getInt(1);
-					String writer = rs.getString(2);
-					String content = rs.getString(3);
-					Timestamp write_date = rs.getTimestamp(4);
-					int thumbs_up = rs.getInt(5);
-					int board_seq = rs.getInt(6);
-					list.add(new ReplyDTO(seq, writer, content, write_date, thumbs_up ,board_seq));
+		if (order_by.equals("write_date") || order_by.equals("thumbs_up")) {
+			// SQL 인젝션 방지를 위해 허용된 매개 변수 값인지 검사 후 코드 실행
+			String sql = "select * from reply where board_seq = ? order by " + order_by + " desc";
+			try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
+				pstat.setInt(1, board_seq);
+				List<ReplyDTO> list = new ArrayList<>();
+				try (ResultSet rs = pstat.executeQuery();) {
+					while (rs.next()) {
+						int seq = rs.getInt(1);
+						String writer = rs.getString(2);
+						String content = rs.getString(3);
+						Timestamp write_date = rs.getTimestamp(4);
+						int thumbs_up = rs.getInt(5);
+						list.add(new ReplyDTO(seq, writer, content, write_date, thumbs_up, board_seq));
+					}
+					return list;
 				}
-				return list;
 			}
 		}
+		// 허용되지 않은 매개 변수의 값인 경우 null 값 반환
+		return null;
 	}
 
 	// 댓글 작성
 	public boolean insert(ReplyDTO dto) throws Exception {
-		
+
 		String sql = "insert into reply values (reply_seq.nextval, ?, ?, sysdate, 0, ?)";
 
 		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
@@ -72,8 +78,8 @@ public class ReplyDAO {
 
 	// 댓글 삭제
 	public boolean deleteBySeq(int seq) throws Exception {
-		
-		String sql = "delete from reply where seq = ?";
+
+		String sql = "delete from reply where reply_seq = ?";
 
 		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
 			pstat.setInt(1, seq);
@@ -81,13 +87,13 @@ public class ReplyDAO {
 				return true;
 		}
 		return false;
-		
+
 	}
 
 	// 댓글 수정
 	public boolean updateContent(int seq, String content) throws Exception {
-		
-		String sql = "update reply set content = ? where seq = ?";
+
+		String sql = "update reply set content = ? where reply_seq = ?";
 
 		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
 			pstat.setString(1, content);
@@ -97,11 +103,11 @@ public class ReplyDAO {
 		}
 		return false;
 	}
-	
+
 	// 댓글 좋아요
 	public boolean replyLike(int seq) throws Exception {
-		
-		String sql = "update reply set thumbs_up = thumbs_up + 1 where seq = ?";
+
+		String sql = "update reply set thumbs_up = thumbs_up + 1 where reply_seq = ?";
 
 		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
 			pstat.setInt(1, seq);
@@ -110,11 +116,11 @@ public class ReplyDAO {
 		}
 		return false;
 	}
-	
+
 	// 댓글 좋아요 취소
 	public boolean replyUnLike(int seq) throws Exception {
-		
-		String sql = "update reply set thumbs_up = thumbs_up - 1 where seq = ?";
+
+		String sql = "update reply set thumbs_up = thumbs_up - 1 where reply_seq = ?";
 
 		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
 			pstat.setInt(1, seq);
@@ -122,5 +128,18 @@ public class ReplyDAO {
 				return true;
 		}
 		return false;
+	}
+
+	// 댓글 좋아요 수를 반환하는 메서드
+	public int selectLikesBySeq(int reply_seq) throws Exception {
+		
+		String sql = "select thumbs_up from reply where reply_seq = ?";
+		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
+			pstat.setInt(1, reply_seq);
+			try (ResultSet rs = pstat.executeQuery();) {
+				rs.next();
+				return rs.getInt(1);	
+			}
+		}
 	}
 }
