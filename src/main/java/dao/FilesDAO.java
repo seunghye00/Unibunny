8828991cf15f19d783ad2a6926293c13df85b3dd
seunghye00 <tmdgp1934@gmail.com>
2024-06-23 18:52:25 +0900

    FilesDAO 파일 변경
    게시글 상세 페이지에서 조회할 file list 반환 메서드 추가

--- src/main/java/dao/FilesDAO.java
+++ src/main/java/dao/FilesDAO.java
@@ -1,10 +1,18 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.google.gson.JsonElement;
+
+import dto.FilesDTO;
 
 public class FilesDAO {
 
@@ -23,5 +31,25 @@ public class FilesDAO {
 		Context ctx = new InitialContext();
 		DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/oracle");
 		return ds.getConnection();
+	}
+
+	// board_seq로 해당 게시물의 파일 목록 조회
+	public List<FilesDTO> selectByBoardSeq(int board_seq) throws Exception {
+		
+		String sql = "select * from files where board_seq = ?";
+
+		List<FilesDTO> list = new ArrayList<>();
+		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
+			pstat.setInt(1, board_seq);
+			try (ResultSet rs = pstat.executeQuery();) {
+				while (rs.next()) {
+					int file_seq = rs.getInt(1);
+					String oriname = rs.getString(2);
+					String sysname = rs.getString(3);
+					list.add(new FilesDTO(file_seq, oriname, sysname, board_seq));
+				}
+				return list;
+			}
+		}
 	}
 }
