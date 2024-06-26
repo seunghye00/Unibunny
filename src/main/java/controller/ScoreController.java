e319c2d4dcd987114972b58c79403b320bb98fe1
Moontari <shaaa6256@gmail.com>
2024-06-26 10:06:43 +0900

    커뮤니티, 랭킹 수정사항 반영
    랭킹 페이지 게임 별 조회
    커뮤니티 사이트 셀렉트 옵션 값 조정

--- src/main/java/controller/ScoreController.java
+++ src/main/java/controller/ScoreController.java
@@ -2,7 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Timestamp;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import dao.ScoreDAO;
 import dto.ScoreDTO;
@@ -25,24 +26,34 @@ public class ScoreController extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 		
 		// JSON 라이브러리
+		Gson gson = new GsonBuilder().setDateFormat("yyyy.MM.dd").create();
 		Gson g = new Gson();
-		
 		// reponse writer 변수 저장
 		PrintWriter pw = response.getWriter();
 		
 		// 접속 경로 저장
 		String cmd = request.getRequestURI();
-		// System.out.println(cmd);
+		System.out.println(cmd);
 		ScoreDAO dao = ScoreDAO.getInstance();
 		try {
-			if(cmd.equals("/submit.score")) {
+			if(cmd.equals("/list.score")){
+				int gamelistNum = Integer.parseInt(request.getParameter("gamelistNum"));
+				System.out.println(gamelistNum);
+				List<ScoreDTO> list = dao.selectListGame(gamelistNum);
+				String jsonResult = gson.toJson(list);
+				pw = response.getWriter();
+				pw.append(jsonResult);
+				pw.flush();
+				pw.close();
+			}
+			else if(cmd.equals("/submit.score")) {
 				String loginID = (String)request.getSession().getAttribute("loginID");
 				// loginID에 임시 데이터 대입 => 이후에 삭제할 코드
 				if(loginID == null) {
 					loginID = "user001";
 				}
-				System.out.println(request.getParameter("score"));
-				System.out.println(request.getParameter("gameId"));
+				//System.out.println(request.getParameter("score"));
+				//System.out.println(request.getParameter("gameId"));
 				int score = Integer.parseInt(request.getParameter("score"));
 				int gameId = Integer.parseInt(request.getParameter("gameId"));
 				pw.append(g.toJson(gameId));
